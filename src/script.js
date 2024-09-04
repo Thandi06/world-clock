09a279ecb2baaf890766cf1fe1ecc51b9e0ddc3d
Thandi Zikhokhile Sithole <cokejobe@gmail.com>
2024-09-04 15:11:45 +0200

    final world clock project

--- src/script.js
+++ src/script.js
@@ -56,6 +56,7 @@ function updateCity(event){
         </div>
         <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
     </div>
+    <a href="index.html">Back to homepage</a>
     `;
 }
 updateTime();
